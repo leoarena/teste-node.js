94c33f66f3fd7125e50bf4b384128734e0d45232
Leonardo Arena <leonardoarena7@gmail.com>
2023-05-10 22:01:33 -0300

    módulo analisarVelocidade

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ const somar = require("./moduloSomar");
 
 let value = somar(10, 2);
 console.log(value);
+
+const analisarVelocidade = require("./moduloVelocidade");
+
+console.log(analisarVelocidade(12, 10));
