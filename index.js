71a81a9456feb9efce80a720c899c57c756208cb
Leonardo Arena <leonardoarena7@gmail.com>
2023-05-12 10:33:02 -0300

    módulo buscar usuário github

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 // const modulo = require("./moduloExemplo");
-
 // modulo.executa();
 // modulo.escreve();
 // modulo.oculta();
 
-const { executa, escreve, oculta } = require("./moduloExemplo");
+// const { executa, escreve, oculta } = require("./moduloExemplo");
+// executa();
+// escreve();
+// oculta();
 
-executa();
-escreve();
-oculta();
+// const somar = require("./moduloSomar");
+// console.log(somar(10, 2));
 
-const somar = require("./moduloSomar");
+// const analisarVelocidade = require("./moduloVelocidade");
+// console.log(analisarVelocidade(12, 10));
 
-let value = somar(10, 2);
-console.log(value);
-
-const analisarVelocidade = require("./moduloVelocidade");
-
-console.log(analisarVelocidade(12, 10));
+const usuarioGithub = require("./moduloGithub");
+usuarioGithub("leoarena")
+  .then((resultado) => {
+    console.log(resultado);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
